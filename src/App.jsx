29bca900f331386ff5d6280f74d2a4641d49d84e
Imran Ahmed <iam.imran@yahoo.com>
2023-11-22 21:02:04 +0500

    adds two components 1. Header 2. Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import "./App.css";
-
 function App() {
   return (
     <>
-      <h1 className="text-3xl">Welcome! to react router dom</h1>
+      <h1 className="text-3xl text-blue-700">Welcome! to react router dom</h1>
     </>
   );
 }

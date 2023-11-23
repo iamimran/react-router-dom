5c021fb5a771c2c801b43054707e9e5d13d4c208
Imran Ahmed <iam.imran@yahoo.com>
2023-11-23 16:22:03 +0500

    implements react router dom to navigate between multiple components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
+import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-import Home from "./components/Home/Home";
 
-function App() {
+export default function App() {
   return (
     <>
       <Header />
-      <Home />
+      <Outlet />
       <Footer />
     </>
   );
 }
-
-export default App;
